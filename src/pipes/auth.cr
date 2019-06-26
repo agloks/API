89df22c3b79389c2bc2bf472e77a40575d712fda
Amelie Certin <amelie.certin@gmail.com>
2019-06-26 22:14:45 +0200

    Call next if OPTION

--- src/pipes/auth.cr
+++ src/pipes/auth.cr
@@ -1,11 +1,14 @@
 module Pipes
   class Auth < Amber::Pipe::Base
     def call(context)
+      call_next(context) if context.request.method == "OPTION"
       begin
         token = context.request.headers["JWT"]
         data = ::Auth::JWTService.new.decode(token)
       rescue JWT::VerificationError
         return not_auth(context)
+      rescue KeyError
+        return missing_JWT(context)
       end
 
       return not_auth(context) if context.session[:current_user_id] != data[0]["user_id"].to_s
@@ -15,7 +18,13 @@ module Pipes
 
     private def not_auth(context)
       context.response.status_code = 404
-      error = {error: "Please Sign In"}.to_json
+      error = {errors: ["Please Sign In"]}.to_json
+      context.response.print error
+    end
+
+    private def missing_JWT(context)
+      context.response.status_code = 403
+      error = {errors: ["Missing JWT headers"]}.to_json
       context.response.print error
     end
   end
