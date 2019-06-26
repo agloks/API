505fa3917ef72435c802735c362f6ebc101db421
Amelie Certin <amelie.certin@gmail.com>
2019-06-26 22:47:36 +0200

    OPTIONS

--- src/pipes/auth.cr
+++ src/pipes/auth.cr
@@ -1,7 +1,7 @@
 module Pipes
   class Auth < Amber::Pipe::Base
     def call(context)
-      call_next(context) if context.request.method == "OPTION"
+      call_next(context) if context.request.method == "OPTIONS"
       begin
         token = context.request.headers["JWT"]
         data = ::Auth::JWTService.new.decode(token)
