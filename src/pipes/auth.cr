3fb2d3df0f09331d7a844f6210cfcd72b6454ec9
Amelie Certin <amelie.certin@gmail.com>
2019-06-27 12:15:39 +0200

    Add logs

--- src/pipes/auth.cr
+++ src/pipes/auth.cr
@@ -11,13 +11,15 @@ module Pipes
         return missing_JWT(context)
       end
 
+      pp data
+
       return not_auth(context) if context.session[:current_user_id] != data[0]["user_id"].to_s
 
       call_next(context)
     end
 
     private def not_auth(context)
-      context.response.status_code = 404
+      context.response.status_code = 403
       error = {errors: ["Please Sign In"]}.to_json
       context.response.print error
     end
