2af7c3fe405acbb16ac437a215aafb2deda380e9
Amelie Certin <amelie.certin@gmail.com>
2019-07-11 23:28:39 +0200

    Do not run CORS on Android

--- src/pipes/cors.cr
+++ src/pipes/cors.cr
@@ -1,12 +1,22 @@
 module Pipes
-  class CORS < Amber::Pipe::Base
+  class CORS < Amber::Pipe::CORS
     def call(context)
-      if context.request.method == "OPTIONS" &&
-         !context.request.headers["Access-Control-Request-Headers"].includes? "content-type"
-        context.request.headers["Access-Control-Request-Headers"] += ",content-type"
+      if android?(context.request)
+        call_next(context)
+      else
+        if preflight?(context) && !content_type?(context.request)
+          context.request.headers[Amber::Pipe::Headers::REQUEST_HEADERS] += ",content-type"
+        end
+        super
       end
+    end
 
-      call_next(context)
+    private def android?(request) : Bool
+      request.headers["source"]? == "android"
+    end
+
+    private def content_type?(request) : Bool
+      request.headers[Amber::Pipe::Headers::REQUEST_HEADERS].includes? "content-type"
     end
   end
 end
