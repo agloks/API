efd15c2a55d2effaa5351bed841705141159500c
Amelie Certin <amelie.certin@gmail.com>
2019-07-13 22:45:29 +0200

    Remove unused methods

--- src/controllers/game_controller.cr
+++ src/controllers/game_controller.cr
@@ -18,67 +18,6 @@ class GameController < ApplicationController
     end
   end
 
-  # def index
-  #   questions = Question.where(media_id: params["media_id"]).select
-  #   respond_with do
-  #     json questions.to_json
-  #   end
-  # end
-  #
-  # def show
-  #   respond_with do
-  #     json question.to_json
-  #   end
-  # end
-  #
-  # def create
-  #   question = Question.new creation_question_params.validate!
-  #   if question.save
-  #     respond_with(201) do
-  #       json question.to_json
-  #     end
-  #   else
-  #     respond_with(403) do
-  #       json({errors: formatted_errors(question)}.to_json)
-  #     end
-  #   end
-  # end
-  #
-  # def update
-  #   question.set_attributes update_question_params.validate!
-  #   if question.save
-  #     respond_with do
-  #       json question.to_json
-  #     end
-  #   else
-  #     respond_with(403) do
-  #       json({errors: formatted_errors(question)}.to_json)
-  #     end
-  #   end
-  # end
-  #
-  # def destroy
-  #   question.destroy
-  #   respond_with(204) do
-  #     json ""
-  #   end
-  # end
-  #
-  # private def creation_question_params
-  #   params.validation do
-  #     required :content
-  #     required :media_id
-  #     required :answers
-  #   end
-  # end
-  #
-  # private def update_question_params
-  #   params.validation do
-  #     optional :content
-  #     optional :answers
-  #   end
-  # end
-  #
   private def set_lobby
     @lobby = Lobby.find! params[:lobby_id]
   end
