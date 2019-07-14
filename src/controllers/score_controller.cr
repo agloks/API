4be473f5ca7394c314ebd9e3e42296f7c9d81a41
Amelie Certin <amelie.certin@gmail.com>
2019-07-14 22:23:14 +0200

    Creat score controller

---
+++ src/controllers/score_controller.cr
@@ -0,0 +1,20 @@
+class ScoreController < ApplicationController
+  def index
+    query = "JOIN games_users gu ON gu.user_id = users.id \
+      JOIN games ON gu.game_id = games.id \
+      WHERE games.lobby_id = ?"
+    users = User.all(query, [params["id"]]).each_with_object({} of Int64 => String) do |user, hash|
+      hash[user.id.not_nil!] = user.nickname.not_nil!
+    end
+
+    query = "JOIN games ON games.id = scores.game_id \
+      WHERE games.lobby_id = ?"
+    query += " AND games.created_at >= now()::date" if params["daily"]?
+    scores = Score.all(query, [params["id"]]).group_by { |score| score.user_id }
+      .map { |id, scores| {users[id] => scores.sum { |s| s.points || 0 }} }[0]
+
+    respond_with do
+      json scores.to_json
+    end
+  end
+end
