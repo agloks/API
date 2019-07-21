478a9475bd256b5f843ca5dc5638b486950e6ee2
Amelie Certin <amelie.certin@gmail.com>
2019-07-21 12:07:52 +0200

    Order scores

--- src/controllers/score_controller.cr
+++ src/controllers/score_controller.cr
@@ -10,12 +10,19 @@ class ScoreController < ApplicationController
     query = "JOIN games ON games.id = scores.game_id \
       WHERE games.lobby_id = ?"
     query += " AND games.created_at >= now()::date" if params["daily"]?
-    scores = Score.all(query, [params["id"]]).group_by { |score| score.user_id }.map do |id, scores|
+    scores = Score.all(query, [params["id"]]).group_by { |score| score.user_id }
+    scores[current_user_id] = [Score.new(points: 0)] unless scores[current_user_id]?
+
+    response = scores.map do |id, scores|
       {"score" => scores.sum { |s| s.points || 0 }, "nickname" => users[id], "id" => id}
-    end
+    end.sort { |a, b| b["score"].as(Int32) <=> a["score"].as(Int32) }
 
     respond_with do
-      json scores.to_json
+      json response.to_json
     end
+  end
+
+  private def current_user_id
+    session["current_user_id"].not_nil!.to_i
   end
 end
