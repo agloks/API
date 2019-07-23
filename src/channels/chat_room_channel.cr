8aa452de28d81d1c6e436ddd095e5e6816538e5a
Amelie Certin <amelie.certin@gmail.com>
2019-07-23 05:36:00 +0200

    display

--- src/channels/chat_room_channel.cr
+++ src/channels/chat_room_channel.cr
@@ -25,18 +25,20 @@ class ChatRoomChannel < Amber::WebSockets::Channel
 
     Message.create(content: answer, lobby_id: lobby_id, user_id: user.id)
 
-    if !running_game.nil? && payload["question_id"]?
-      question = Question.find payload["question_id"].to_s
-
-      if !question.nil?
-        score = 25
-        store_score(user.id, question.id, running_game.id, score)
-      end
-    else
-      score = ""
-    end
-
-    sleep 3
+    score = "game en cours: #{!running_game.nil?}, question_id: #{payload["question_id"]?}, question existe: #{!question.nil?}"
+    # if !running_game.nil? && payload["question_id"]?
+    #   question = Question.find payload["question_id"].to_s
+    #
+    #   if !question.nil?
+    #     score = JSON.parse(question.answers.not_nil!).as_a.map do |a|
+    #       points = 200 - 200 / a.as_s.size * Levenshtein.distance(a.as_s, answer)
+    #       points.positive? ? points : 0
+    #     end.max
+    #     store_score(user.id, question.id, running_game.id, score)
+    #   end
+    # else
+    #   score = ""
+    # end
 
     string = JSON.build do |json|
       json.object do
@@ -51,7 +53,7 @@ class ChatRoomChannel < Amber::WebSockets::Channel
                 json.field "nickname", user.nickname
               end
             end
-            json.field "score", ""
+            json.field "score", score.to_s
             json.field "content", answer
           end
         end
